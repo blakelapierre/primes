2aefa72c1428877a8e04ee6a9bf836c19c36eb64
Blake La Pierre <blakelapierre@gmail.com>
2016-05-18 03:18:07 -0700

    Generates the first 8.5 million primes in 1.25 minutes...when node runs out of heap space

--- src/tests/index.js
+++ src/tests/index.js
@@ -1,27 +1,43 @@
-function nextPrime(lastPrime, queue) {
-  let i = lastPrime + 1,
-      factors = queue[i];
+// function nextPrime(lastPrime, queue) {
+//   let i = lastPrime + 1,
+//       factors = queue[i];
 
-  console.log(queue.length);
+//   console.log(queue.length);
 
-  while (factors !== undefined) {
-    factors.forEach(requeue);
-    queue[i].splice(0);
-    // delete queue[i];
+//   while (factors !== undefined) {
+//     factors.forEach(requeue);
+//     queue[i].splice(0);
+//     // delete queue[i];
 
-    i++;
+//     i++;
 
-    factors = queue[i];
-  }
+//     factors = queue[i];
+//   }
 
-  requeue(i);
+//   requeue(i);
 
-  return i;
+//   return i;
 
-  function requeue(factor) {
-    (queue[i + factor] = queue[i + factor] || []).push(factor);
-  }
-}
+//   function requeue(factor) {
+//     (queue[i + factor] = queue[i + factor] || []).push(factor);
+//   }
+// }
+
+// function primesTo(x) {
+//   const primes = [];
+//   const queue = [];
+
+//   let prime = 1;
+
+//   while (prime < x) {
+//     prime = nextPrime(prime, queue);
+//     primes.push(prime);
+//   }
+
+//   return primes;
+// }
+
+// // console.log(primesTo(2e10));
 
 function * primeGenerator() {
   yield 1;
@@ -33,37 +49,41 @@ function * primeGenerator() {
   const queue = {};
 
   while (true) {
-    const factors = queue[check];
+    const _ = queue[check];
 
-    if (factors !== undefined) requeue(factors, queue, check);
-    /*jshint -W093 */
-    else {
-      const nextCheck = !((check + check) & 1) ? check + check + check : check + check;
-      (queue[nextCheck] = queue[nextCheck] || []).push(check);
-
-      yield lastPrime = check;
+    if (_ !== undefined) {
+      reinsert(_, queue, check);
+      delete queue[check];
     }
+    else yield reinsert(check, queue, check);
 
-    check = check + 2;
+    check = check + 2; // if we track where the next item in queue is, this can probably be sped up quite a bit
   }
 
-  function requeue(factors, queue, check) {
-    for (let i = 0; i < factors.length; i++) {
-      const factor = factors[i];
-      const nextCheck = !((check + factor) & 1) ? check + factor + factor : check + factor;
-      // could check if a multiple of any queued items...or something?
-      (queue[nextCheck] = queue[nextCheck] || []).push(check);
-    }
-    delete queue[check];
+  function reinsert(i, queue, check) {
+    const nextSlot = check + (((i + check) & 1) ? i : i + i);
+          candidate = queue[nextSlot];
+
+    if (candidate) reinsert(i, queue, check + i); // will eventually stack overflow unless optimized out...?
+    else queue[nextSlot] = i;
+
+    return check;
   }
 }
 
 const generator = primeGenerator();
 
+const check = [1,2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59,61,67,71,73,79,83,89,97,101,103,107,109,113,127,131,137,139,149,151,157,163,167,173,179,181,191,193,197,199,211,223,227,229,233,239,241,251,257,263,269,271,277,281,283,293,307,311,313,317,331,337,347,349,353,359,367,373,379,383,389,397,401,409,419,421,431,433,439,443,449,457,461,463,467,479,487,491,499,503,509,521,523,541];
+let checkPosition = 0;
+
 let logLevel = 1,
     threshold = Math.pow(10, logLevel);
 while (true) {
   const result = generator.next().value;
+
+  if (checkPosition < check.length) {
+    if (check[checkPosition++] !== result) throw new Error(`Not prime! ${result}`);
+  }
 
   console.log(result);
 
@@ -73,19 +93,3 @@ while (true) {
     threshold = Math.pow(10, logLevel);
   }
 }
-
-function primesTo(x) {
-  const primes = [];
-  const queue = [];
-
-  let prime = 1;
-
-  while (prime < x) {
-    prime = nextPrime(prime, queue);
-    primes.push(prime);
-  }
-
-  return primes;
-}
-
-// console.log(primesTo(2e10));
